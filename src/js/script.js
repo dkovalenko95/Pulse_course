fb87c556ec98f8f4a21120fb26f333cc63126adc
Dmytro <example@gmail.com>
2021-04-13 17:53:23 +0300

    adaptive slider done

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,34 @@
 $(document).ready(function(){
     $('.carousel__slider').slick({
+        arrows: true,
+        dots: false,
+        speed: 1200,
+        slidesToShow: 1,
+        adaptiveHeight: false,
+        responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                dots: true
+            }
+        }
+        ]
+    });
+});
+
+/* $(document).ready(function(){
+    $('.carousel__slider').slick({
         speed: 1000,
         arrows: true,
 		dots: false,
-        slidesToShow: 1,
+        slidesToShow: 1, */
+
         // adaptiveHeight: true,
        /*  prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left_red_arrow.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/right_red_arrow.png"></button>', */
-        responsive: [
+        
+        /* responsive: [
             {
             breakpoint: 768,
                 settings: {
@@ -24,4 +45,19 @@ $(document).ready(function(){
             }
         ]
     });
+}); */
+
+/* const slider = tns({
+    container: '.carousel__slider',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false
 });
+
+
+document.querySelector('.prev').onclick = function () {
+    slider.goTo('prev');
+};
+document.querySelector('.next').onclick = function () {
+    slider.goTo('next');
+}; */
