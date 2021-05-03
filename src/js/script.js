17e73346b9730953a2d642c2b9cab4ef5d06efab
Dmytro <example@gmail.com>
2021-05-03 12:50:53 +0300

    validation + mask-number done

--- src/js/script.js
+++ src/js/script.js
@@ -77,6 +77,42 @@ $(document).ready(function() {
             $('.overlay, #order').fadeIn('slow');
         });
     });
+
+    // Validation forms
+
+    function validateForm(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введети минимум {0} символа")
+                },
+                phone: "Пожалуйста введите свой номер телефна",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    }
+    
+    validateForm('#consultation-form');
+    validateForm('#consultation form');
+    validateForm('#order form');
+
+    // Phone number
+    $('input[name=phone]').mask("+38 (999) 999-9999");
 });
 
 /* $(document).ready(function(){
